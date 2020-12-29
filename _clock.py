eff9b2b69dc9e104d340c2c8eb3600c2ee6a55ba
wweny <pocarisweatbot@gmail.com>
2020-12-29 20:12:18 +0900

    po

---
+++ _clock.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from apscheduler.schedulers.blocking import BlockingScheduler
+import os
+import TextTweet
+import GetTweet
+
+# APIのトークン
+CK=os.environ["CK"]
+CS=os.environ["CS"]
+AT=os.environ["AT"]
+AS=os.environ["AS"]
+
+twische = BlockingScheduler(timezone='UTC')
+
+def GT():
+    GetTweet.gettweet(CK,CS,AT,AS)
+
+    #data.txtに保存
+    f = open("Data.txt",encoding="utf-8")
+    text = f.read()
+    f.close()
+
+def TT():
+    TextTweet.puttweet()
+
+twische.add_job(GT,'cron',hour="0-2,7-23",minutes="0,15,30,45")
+twische.add_job(TT,'cron',hour="0-2,7-23",minutes="0,30")
+
+if __name__ == "__main__":
+    twische.start()
