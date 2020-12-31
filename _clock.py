b8dc3d64ec0ec5f4d3ae9d66cb50a14adfa6a01e
wweny <pocarisweatbot@gmail.com>
2020-12-31 14:49:02 +0900

    po

--- _clock.py
+++ _clock.py
@@ -26,5 +26,7 @@ def TT():
 twische.add_job(GT,'cron',hour="0-17,22-23",minute="0,15,30,45")
 twische.add_job(TT,'cron',hour="0-17,22-23",minute="0,30")
 
+GT()
+
 if __name__ == "__main__":
     twische.start()
